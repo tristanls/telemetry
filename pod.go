177f2ac3ee7c41be49c3779a5ea5fe68a5104fe8
Tristan Slominski <tristan.slominski@gmail.com>
2017-02-23 12:20:39 -0600

    Problem: Forgot to include Metrics in Pod.
    Solution: Added Metrics to Pod.

--- pod.go
+++ pod.go
@@ -2,6 +2,7 @@ package telemetry
 
 import (
 	"github.com/tristanls/telemetry/logger"
+	"github.com/tristanls/telemetry/metrics"
 )
 
 // Pod is a bundle of Telemetry-related functionality.
@@ -9,13 +10,15 @@ type Pod struct {
 	Telemetry *Telemetry
 	Emitter   *Emitter
 	Log       *logger.Logger
+	Metrics   *metrics.Metrics
 }
 
 // Constructs a new Telemetry Pod.
-func NewPod(telemetry *Telemetry, emitter *Emitter, log *logger.Logger) *Pod {
+func NewPod(telemetry *Telemetry, emitter *Emitter, log *logger.Logger, metrics *metrics.Metrics) *Pod {
 	return &Pod{
 		Telemetry: telemetry,
 		Emitter:   emitter,
 		Log:       log,
+		Metrics:   metrics,
 	}
 }
